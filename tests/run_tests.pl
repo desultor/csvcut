df063f1a8b622d3365adcbc1a26c3c37d137fcd5
Jesse Ross <jesse.ross@gmail.com>
2014-02-16 14:20:04 -0500

    function factory for clarity

--- tests/run_tests.pl
+++ tests/run_tests.pl
@@ -6,27 +6,52 @@ use strict;
 # I don't see a way to do testing without installing extra python packages, so
 # for now I'm just doing it with this.  
 
-my @tests;
-
-my $test1 = sub {
-  print "foo";
-};
-
-#push @tests, $test1;
-push @tests, (
-  sub {  
-    print "Cut out 'foo' from a single tsv file: ";
-	my $result = `csvcut foo test_sets/sample_csv.tsv`;
-	open OUT, ">script_outputs/foo_tsv.result" or die "can't: $!";
+# build a function reference for a function to test a particular invocation of
+# the script
+sub command_test {
+  my ($explanation_string, # the plaintext describing the test
+      $command, # the command to be tested
+	  $resultfile) # the name of the result file (same as expected results file)
+	  = @_;
+  return sub {  
+    print "$explanation_string: ";
+	my $result = `$command`;
+	open OUT, ">script_outputs/$resultfile" or die "can't: $!";
 	print OUT $result;
 	close OUT;
-	my $foo = system("diff script_outputs/foo_tsv.result expected_results/foo_tsv.result");
+	my $foo = system("diff script_outputs/$resultfile expected_results/$resultfile");
 	if ($foo == 0) {
 	  print "SUCCESS\n";
 	} else {
 	  print "FAILURE\n";
 	}
   },
+}
+
+
+# create a list of the test functions
+my @tests;
+push @tests, (
+  command_test("Cut out column 'foo' from a single tsv file",
+			   "csvcut foo test_sets/sample_csv.tsv",
+			   "tsv.result"),
+  command_test("Cut out column 'foo' from all the tsv files",
+			   "csvcut foo test_sets/*.tsv",
+			   "multiple_tsv.result"),
+  command_test("Cut out column 'two words' from a tsv file",
+			   "csvcut 'two words' test_sets/sample_csv.tsv",
+			   "two_words_tsv.result"),
+  command_test("Cut out column 'bar' from a csv file",
+			   "csvcut bar test_sets/sample_csv.csv --delimiter ,",
+			   "csv.result"),
+  command_test("Delimiter argument at the front of the command",
+			   "csvcut --delimiter , bar test_sets/sample_csv.csv",
+			   "csv_delimiter_front.result"),
+  command_test("Delimiter argument in the middle of the command",
+			   "csvcut bar --delimiter , test_sets/sample_csv.csv",
+			   "csv_delimiter_middle.result"),
 );
 
+# run the test functions
 map {&{$_}} @tests;
+
